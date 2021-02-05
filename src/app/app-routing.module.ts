e3c1899056b8fd4abf76a9aa32fb2bf5ca44ad4c
aturancetin <aturancetinn@gmail.com>
2021-02-05 21:29:42 +0300

    App component modified

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
+import { AboutComponent } from './components/pages/about/about.component';
+import { TodosComponent } from './components/todos/todos.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  { path: '', component: TodosComponent},
+  { path: 'about', component: AboutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
